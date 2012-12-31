f9303b4818fd415309982f78f33fc11b0c16ee34
Cheeseum <z.cheeseum@gmail.com>
2012-12-31 14:37:20 -0700

    refactor stuff around

--- nw-silly-app/main.js
+++ nw-silly-app/main.js
@@ -1,50 +1,18 @@
-irc = require('irc')
+irc = require('nw-irc')
+ircui = require('nw-irc-ui')
 
-nickname = 'nicknamegoeshere'
 clients = {}
+channel_panes = {}
 
-function getChannelPane (server, channel) {
-    return $('#' + server + '\\' + channel)
+ui = new ircui.ClientUI();
+
+exports.addServer (server, opts) {
+    c = irc.Client.init(server, opts);
+    ui.initClient(c);
+    clients[server] = c;
 }
 
-function connect (server, port, ssl) {
-    c = new irc.Client(server, nickname, {
-        'port': port ? port : 6667,
-        'secure': ssl ? true : false,
-    });
-
-    // set up event handlers
-    c.on('message', function(dest, to, text, message) {
-        if (dest === nickname) {
-            // handle private message
-        }
-    });
-
-    c.on('message#', function(dest, to, text, message) {
-        
-    });
-
-    c.on('join', function(channel, nick, message) {
-        if (nick == nickname) {
-            $('<li><a href="#">' + channel + '</a></li>').appendTo('#channel-nav');
-            $('<div class="tab-pane" id="' + server + '\\' + channel + '"></div>').appendTo('#channel-tabs')
-        }
-    });
-
-    c[server] = c;
-}
-
-function joinChannel (server, channel) {
-    // doesn't look like this supports channels with keys
-    // might have to replace this func
-    c[server].join(channel)
-}
-
-function partChannel (server, channel) {
-    c[server].part(channel)
-}
-
-function cycleChannel (server, channel) {
-    c[server].join(channel)
-    c[server].part(channel)
+exports.connectToServer (server) {
+    if (clients[server])
+        clients[server].connect();
 }
