84f0203f3dafb28ca9918037053d7ce0f3a69a14
Cheeseum <z.cheeseum@gmail.com>
2012-12-31 14:44:39 -0700

    fix some oopses

--- nw-silly-app/main.js
+++ nw-silly-app/main.js
@@ -6,13 +6,13 @@ channel_panes = {}
 
 ui = new ircui.ClientUI();
 
-exports.addServer (server, opts) {
+function addServer (server, opts) {
     c = irc.Client.init(server, opts);
     ui.initClient(c);
     clients[server] = c;
 }
 
-exports.connectToServer (server) {
+function connectToServer (server) {
     if (clients[server])
         clients[server].connect();
 }
