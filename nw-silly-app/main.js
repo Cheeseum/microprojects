58884d1ed0a35f3a26ad986dd0140a8e65043778
Cheeseum <z.cheeseum@gmail.com>
2013-01-01 14:52:55 -0700

    jslint and syntatic cleanup

--- nw-silly-app/main.js
+++ nw-silly-app/main.js
@@ -1,34 +1,36 @@
 global.$ = $;
 
-irc = require('nw-irc')
-ircui = require('nw-irc-ui')
+var irc = require('nw-irc');
+var ircui = require('nw-irc-ui');
 
 var clients = {};
 
 var ui = new ircui.ClientUI();
 
-function addServer (server, nickname, opts) {
-    c = new irc.Client(server, nickname, opts);
+function addServer(server, nickname, opts) {
+    var c = new irc.Client(server, nickname, opts);
     ui.initClient(c);
     clients[server] = c;
 }
 
-function connectToServer (server) {
-    if (clients[server])
+function connectToServer(server) {
+    if (clients[server]) {
         clients[server].connect();
+    }
 }
 
-onload = function () {
+window.onload = function () {
     $('#server-menu').fadeIn();
     // TODO: form validation
     $('#server-menu').find('button').on('click', function (e) {
         addServer(
-            $('#inputServerAddress').val(),
-            $('#inputServerNickname').val(),
-            { port: $('#inputServerPort').val(),
-              selfSigned: true,
-              secure: $('#checkboxServerSecure:checked').length,
-              password: $('#inputServerPassword').val()
+            $('#input-server-address').val(),
+            $('#input-server-nickname').val(),
+            {
+                port: $('#input-server-port').val(),
+                selfSigned: true,
+                secure: $('#checkbox-server-secure:checked').length,
+                password: $('#input-server-password').val()
             }
         );
         $('#server-menu').find('form')[0].reset();
